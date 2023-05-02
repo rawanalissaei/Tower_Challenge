c20bd9223b486b512bcb6ee098001014b6683175
rawanalissaei <rawanharib@gmail.com>
2023-05-02 16:34:45 +0100

    Initial commit

---
+++ src/main/java/com/challenge/Tower_Challenge/TowerChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.challenge.Tower_Challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TowerChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TowerChallengeApplication.class, args);
+	}
+
+}
